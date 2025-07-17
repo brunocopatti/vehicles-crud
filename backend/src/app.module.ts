505988dc5fe5ac52c612829ca1bb5771425c3e75
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-07-17 15:55:17 -0300

    feat: add vehicle resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       process.env.MONGODB_URI || 'mongodb://localhost:27017',
     ),
+    VehiclesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
