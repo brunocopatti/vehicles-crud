83821e744b00f9415fd5d5aeab6e29c7986b40b5
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-07-17 16:06:24 -0300

    fix: mongodb connection uri

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { VehiclesModule } from './vehicles/vehicles.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017',
+      process.env.MONGODB_URI || 'mongodb://username:password@localhost:27017',
     ),
     VehiclesModule,
   ],
